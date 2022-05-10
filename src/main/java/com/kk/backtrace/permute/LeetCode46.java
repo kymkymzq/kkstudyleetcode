610bb7970eb831a9684534b93bb9a328096251ff
kk <475893310@qq.com>
2022-05-10 15:38:29 +0800

    回溯法,全排列II,力扣47题

---
+++ src/main/java/com/kk/backtrace/permute/LeetCode46.java
@@ -0,0 +1,69 @@
+package com.kk.backtrace.permute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 46. 全排列  https://leetcode.cn/problems/permutations/
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 示例 3：
+ *
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ */
+
+class LeetCode46 {
+    public static List<List<Integer>> permute(int[] nums) {
+        if(nums.length == 0){
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+
+        dfs(temp, res, nums, 0, visited);
+        return res;
+
+    }
+
+//    public static void dfs(int[] nums, int index, List<Integer> temp, List<List<Integer>> res, boolean[] visited){
+    public static void dfs(List<Integer> temp, List<List<Integer>> res, int[] nums, int index, boolean[] visited){
+        if(index == nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(visited[i]){
+                continue;
+            }
+            visited[i] = true;
+            temp.add(nums[i]);
+            dfs(temp, res, nums, index + 1, visited);//index + 1 :因为每次取完一个数，下一次dfs是从下一个数开始取
+            visited[i] = false;
+            temp.remove(temp.size() - 1);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> permute = permute(nums);
+        System.out.println(permute);
+        /*
+        [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+         */
+    }
+
+}
--- src/main/java/com/kk/backtrace/permute/LeetCode47.java
+++ src/main/java/com/kk/backtrace/permute/LeetCode47.java
@@ -1,69 +1,73 @@
 package com.kk.backtrace.permute;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
- * 46. 全排列  https://leetcode.cn/problems/permutations/
- * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * 47. 全排列 II
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
  *
  * 示例 1：
  *
- * 输入：nums = [1,2,3]
- * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ *  [1,2,1],
+ *  [2,1,1]]
  * 示例 2：
  *
- * 输入：nums = [0,1]
- * 输出：[[0,1],[1,0]]
- * 示例 3：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  *
- * 输入：nums = [1]
- * 输出：[[1]]
+ * 提示：
+ *
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
  */
 
-class Leetcode46 {
-    public static List<List<Integer>> permute(int[] nums) {
+class LeetCode47 {
+    public static List<List<Integer>> permuteUnique(int[] nums) {
         if(nums.length == 0){
             return new ArrayList<>();
         }
+
+        Arrays.sort(nums); //有重复数字的数组 一定要排序！！！！！！
 
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
 
         dfs(temp, res, nums, 0, visited);
+
         return res;
-
     }
-
-//    public static void dfs(int[] nums, int index, List<Integer> temp, List<List<Integer>> res, boolean[] visited){
+    
     public static void dfs(List<Integer> temp, List<List<Integer>> res, int[] nums, int index, boolean[] visited){
         if(index == nums.length){
             res.add(new ArrayList<>(temp));
             return;
         }
-
+        
         for(int i = 0; i < nums.length; i++){
-            if(visited[i]){
+            if(visited[i] || (i > 0 && !visited[i - 1] && nums[i] == nums[i - 1])){
                 continue;
             }
+
             visited[i] = true;
             temp.add(nums[i]);
-            dfs(temp, res, nums, index + 1, visited);//index + 1 :因为每次取完一个数，下一次dfs是从下一个数开始取
+            dfs(temp, res, nums, index + 1, visited);
             visited[i] = false;
             temp.remove(temp.size() - 1);
-
         }
-
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-        List<List<Integer>> permute = permute(nums);
-        System.out.println(permute);
+        int[] nums = {1, 1, 2};
+        List<List<Integer>> lists = permuteUnique(nums);
+        System.out.println(lists);
         /*
-        [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+        [[1, 1, 2], [1, 2, 1], [2, 1, 1]]
          */
     }
-
 }
